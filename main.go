a38fee20e81f678f6ddcabcf88d4cf087817775c
unforgivenvirtuoso <hamza-malik99@hotmail.com>
2022-03-12 19:32:01 +0000

    problem 2 and 3

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	problem1()
+	problem3()
 }
 
+// Find the sum of all the multiples of 3 or 5 below 1000.
 func problem1() {
 
 	n := 1000
@@ -14,11 +15,43 @@ func problem1() {
 	for i := 0; i < n; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			sum += i
+		}
+	}
+	fmt.Println(sum)
 
+}
+
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+// find the sum of the even-valued terms.
+func problem2() {
+	current := 1
+	next := 2
+	even := 0
+
+	for current < 4000000 {
+		fib := current + next
+
+		if fib%2 == 0 {
+			even += fib
 		}
 
+		current = next
+		next = fib
 	}
+	fmt.Println(even)
+}
 
-	fmt.Println(sum)
+// What is the largest prime factor of the number 600851475143
+func problem3() {
+	n := 600851475143
+	prime := 2
+
+	for prime < n {
+		for n%prime == 0 {
+			n = n / prime
+		}
+		prime++
+	}
+	fmt.Println(prime)
 
 }
